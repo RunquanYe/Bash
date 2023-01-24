8161028a6c04f83194de4276b030a3f80d4a30bb
Jerryyk8@gmail.com <yer@mail.gvsu.edu>
2023-01-24 02:40:09 -0800

    update

--- fileHelper.sh
+++ fileHelper.sh
@@ -31,7 +31,7 @@ The FileHelper Bash Script is for renaming a series of files and creating dummy 
     -v,     Display script version.
 
     -r,     Rename series of same type files.(You can also just use -r without any argument, then it will go into read user input mode.)
-                argument$1, a series of targeted rename files, (required, you must use comma \",\" as delimiter, like: t1.txt,t2.txt ).
+                argument$1, a series of targeted rename files, (required, you must use comma \",\" or space as delimiter).
                 argument$2, customized output files' name, (optional, default value: "File" ).
                 argument$3, start ordinal number for the rename sequences, (optional, default value: 1).
     
@@ -59,25 +59,31 @@ EOF
 # @argument: $3, start ordinal number for the rename sequences. 
 #****************************************************************************************************
 renameFile(){
-    _argList=( ${argumentList[@]:1} )
+    declare -a _argList=("$@")
     
     _targetFile=""
     _newFileName=""
     _ordinalNumber=""
-    
-    if [[ ${#_argList[@]} > 0 ]]; then
+   
+    if [[ ${#_argList[@]} -ge 0 ]]; then
         _targetFile=${_argList[0]}
         [[ -z "$_targetFile" ]] &&  printf "\033[34;1mInput Argument Error, Unvalid target file name. Please retry with a valid non-null string value. \033[0m\n"  && return 1
 
-        [[ ! -z "${_argList[1]}" ]] &&  _newFileName=${_argList[1]} || $(  _newFileName="File" && printf "\033[34;1mInput Argument Error, Unvalid customized file name. Go with default output file name, \"File\". \033[0m\n" )
-
-        if [ ! ${_argList[2]} > 0 ]; then _ordinalNumber=1 && printf "\033[34;1mInput Argument Error, Unvalid ordinal number. Go with default initial ordinal number, 1. \033[0m\n"; else _ordinalNumber=$(expr ${_argList[2]} + 0); fi
+        if [ -z "${_argList[1]}" ]; then _newFileName="File"; printf "\033[34;1mInput Argument Error, Unvalid customized file name. Go with default output file name, \"File\". \033[0m\n"; else _newFileName=${_argList[1]}; fi
+        
+        if [[ ! "${_argList[2]}" =~ ^[0-9]+$ ]]; then
+            _ordinalNumber=1
+            printf "\033[34;1mInput Argument Error, Unvalid ordinal number. Go with default initial ordinal number, 1. \033[0m\n"
+        else
+            _ordinalNumber=$(expr ${_argList[2]} + 0)
+            if (( _ordinalNumber < 1 )); then _ordinalNumber=1; printf "\033[34;1mInput Argument Error, Unvalid ordinal number. Go with default initial ordinal number, 1. \033[0m\n"; fi
+        fi
     else
         echo "Please input the target file(s) you want to rename, please use comma \",\" to be delimiter."
         read target
         _targetFile=$target
         while [[ -z "$_targetFile" ]]; do
-            echo "You inputed unvalid string value to be the target file name, please try again  and use comma \",\" to be the delimiter."
+            echo "You inputed unvalid string value to be the target file name, please try again and use comma \",\" or space to be the delimiter."
             read -e target
             _targetFile=$target
         done
@@ -101,7 +107,6 @@ renameFile(){
     fi
     
     _targetFileList=(`echo $_targetFile | tr ',' ' '`)
-    
     echo "Rename $_targetFile to file series format, $( [[ ${_newFileName%.*} == ${_newFileName#*.} ]] && echo "$_newFileName"_X || echo "${_newFileName%.*}_X.${_newFileName#*.}" ):"
 
     for _file in "${_targetFileList[@]}"
@@ -112,11 +117,10 @@ renameFile(){
             mv -- "$_file" "$_new"
             (( _ordinalNumber += 1 ))
         else
-            printf "\033[34;1mError, Unvalid target rename file, $_file, does not exist. \033[0m\n" 
+            printf "\033[34;1mError, Unvalid target rename file, $_file, does not exist.\033[0m\n" 
         fi
     done
 }
-
 
 
 #*************************************************************************************************************************
@@ -127,17 +131,15 @@ renameFile(){
 # @argument: $4, customized output data file name. 
 #*************************************************************************************************************************
 function createDummyFile () {
-    _argList=( ${argumentList[@]:1} )
-
-    _content=$( [ ! -z "${_argList[0]}" ] && echo "${_argList[0]}" || echo "This is test content." )
+    _content=$( [ ! -z $1 ] && echo $1 || echo "This is test content." )
     _linNum=1
 
-    [[ ${_argList[2]} == 'N' || ${_argList[2]} == 'n' || ${_argList[2]} == 0 ]] && _linNum=0 
+    [[ $3 == 'N' || $3 == 'n' || $3 == 0 ]] && _linNum=0 
     # let user input the content as $1, loop times as $2 and output file name as $3.
-    printf "Generate a dummy test data files, ${_argList[3]:-dummyTest.txt}\n"
-    for (( i=0;i<${_argList[1]:-20};i++ ))
+    printf "Generate a dummy test data files, ${4:-dummyTest.txt}\n"
+    for (( i=0;i<${2:-20};i++ ))
     do 
-        echo "$( [[ $_linNum == 1 ]] && echo "$(( $i+1 )): " )$_content" >> ${_argList[3]:-dummyTest.txt}
+        echo "$( [[ $_linNum == 1 ]] && echo "$(( $i+1 )): " )$_content" >> ${4:-dummyTest.txt}
     done 
 }
 
@@ -152,21 +154,19 @@ function createDummyFile () {
 # @argument: $5, number of times generate dummy data files.
 #********************************************************************************************************************************************************************
 function createNumDummyFiles () {
-    _argList=( ${argumentList[@]:1} )
-    
-    _content=$( [[ ! -z "${_argList[0]}" ]] && echo "${_argList[0]}" || echo "This is test content." )
+    _content=$( [[ ! -z $1 ]] && echo $1 || echo "This is test content." )
     _linNum=1
 
-    [[ ${_argList[2]} == 'N' || ${_argList[2]} == 'n' || ${_argList[2]} = 0 ]] && _linNum=0;  
+    [[ $3 == 'N' || $3 == 'n' || $3 = 0 ]] && _linNum=0;  
      
     # let user input the content as $1, loop times as $2 and output file name as $3.
-    printf "Generate a series of dummy test data files, ${_argList[3]:-dummyTest.txt}\n"
-    for (( f=1; f<=${_argList[4]:-5}; f++ ))
+    printf "Generate a series of dummy test data files, ${4:-dummyTest.txt}\n"
+    for (( f=1; f<=${5:-5}; f++ ))
     do    
-        echo "  Generate No.$f test data file, $( [ -z "${_argList[3]}" ] && echo "dummyTest_$f.txt" || echo "${_argList[3]%.*}_$f.${_argList[3]#*.}" )"
-        for (( i=0;i<${_argList[1]:-20};i++ ))
+        echo "  Generate No.$f test data file, $( [ -z $4 ] && echo "dummyTest_$f.txt" || echo "${4%.*}_$f.${4#*.}" )"
+        for (( i=0;i<${2:-20};i++ ))
         do 
-            echo "$( [[ $_linNum = 1 ]] && echo "$(( $i+1 )): " )$_content" >> $( [ -z "${_argList[3]}" ] && echo "dummyTest_$f.txt" || echo "${_argList[3]%.*}_$f.${_argList[3]#*.}" )
+            echo "$( [[ $_linNum = 1 ]] && echo "$(( $i+1 )): " )$_content" >> $( [ -z $4 ] && echo "dummyTest_$f.txt" || echo "${4%.*}_$f.${4#*.}" )
         done 
     done
 }
@@ -192,7 +192,6 @@ displayProgrammerInfo(){
 [ $# -eq 0 ] && printf "\033[34;1mEmpty Command Option. Please look at the Help method below and try the valid option commands. \033[0m\n"  && showHelp
 
 while getopts ":hvrcC" option; do
-    argumentList=( "$@" )  
     case $option in
         h) # display Help.
             showHelp
@@ -201,13 +200,14 @@ while getopts ":hvrcC" option; do
             echo "Version = $version"
             ;;
         r) # call the renameFile funcation to rename series of same type files.
-            renameFile
+            argArray=("$2" $3 $4)
+            renameFile "${argArray[@]}"
             ;;
         c) # call the createDummyFile to generate a dummy test data file.
-            createDummyFile
+            createDummyFile "$2" $3 $4 $5
             ;;
         C) # generate a series of dummy test data files.
-            createNumDummyFiles
+            createNumDummyFiles "$2" $3 $4 $5 $6
             ;;
     esac
 shift
@@ -218,4 +218,4 @@ done
 displayProgrammerInfo
 
 # Exit the program
-exit 0                                  
+exit 0
